8f6fa7fee38ea37f742175641a24db543a6e794b
peizhouyu <peizhouyu@live.com>
2017-11-07 15:42:25 +0800

    配置文件

---
+++ src/main/java/cn/mrpei/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package cn.mrpei.dao;
+
+import cn.mrpei.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
---
+++ src/main/java/cn/mrpei/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package cn.mrpei.dao;
+
+import cn.mrpei.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
