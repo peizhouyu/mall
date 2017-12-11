4346339003a9c9f818e6f8a3be406183a42c22bb
peizhouyu <peizhouyu@live.com>
2017-12-11 22:38:51 +0800

    add RESTful API

--- src/main/java/cn/mrpei/controller/portal/ProductController.java
+++ src/main/java/cn/mrpei/controller/portal/ProductController.java
@@ -4,11 +4,10 @@ import cn.mrpei.common.ServerResponse;
 import cn.mrpei.service.ProductService;
 import cn.mrpei.vo.ProductDetailVo;
 import com.github.pagehelper.PageInfo;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Date:2017/11/14
@@ -29,6 +28,12 @@ public class ProductController {
         return productService.getProductDetail(productId);
     }
 
+    @RequestMapping(value = "/{productId}", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<ProductDetailVo> detailRESTful(@PathVariable("productId") Integer productId){
+        return productService.getProductDetail(productId);
+    }
+
     @RequestMapping("/list.do")
     @ResponseBody
     public ServerResponse<PageInfo> list(@RequestParam(value = "keyword",required = false) String keyword,
@@ -40,5 +45,55 @@ public class ProductController {
         return productService.getProductByKeywordCategory(keyword,categoryId,pageNum,pageSize,orderBy);
     }
 
+    @RequestMapping(value = "/{keyword}/{categoryId}/{pageNum}/{pageSize}/{orderBy}", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTful(@PathVariable(value = "keyword") String keyword,
+                                         @PathVariable(value = "categoryId") Integer categoryId,
+                                         @PathVariable(value = "pageNum") int pageNum,
+                                         @PathVariable(value = "pageSize") int pageSize,
+                                         @PathVariable(value = "orderBy") String orderBy){
+
+        return productService.getProductByKeywordCategory(keyword,categoryId,pageNum,pageSize,orderBy);
+    }
+
+    //http://localhost:8080/product/keyword/手机/1/10/price_asc/
+    @RequestMapping(value = "/keyword/{keyword}/{pageNum}/{pageSize}/{orderBy}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTful(@PathVariable(value = "keyword")String keyword,
+                                                @PathVariable(value = "pageNum") Integer pageNum,
+                                                @PathVariable(value = "pageSize") Integer pageSize,
+                                                @PathVariable(value = "orderBy") String orderBy){
+        if(pageNum == null){
+            pageNum = 1;
+        }
+        if(pageSize == null){
+            pageSize = 10;
+        }
+        if(StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+
+        return productService.getProductByKeywordCategory(keyword,null,pageNum,pageSize,orderBy);
+    }
+
+    //http://localhost:8080/product/category/100012/1/10/price_asc/
+    @RequestMapping(value = "/category/{categoryId}/{pageNum}/{pageSize}/{orderBy}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTful(@PathVariable(value = "categoryId")Integer categoryId,
+                                                @PathVariable(value = "pageNum") Integer pageNum,
+                                                @PathVariable(value = "pageSize") Integer pageSize,
+                                                @PathVariable(value = "orderBy") String orderBy){
+        if(pageNum == null){
+            pageNum = 1;
+        }
+        if(pageSize == null){
+            pageSize = 10;
+        }
+        if(StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+
+        return productService.getProductByKeywordCategory("",categoryId,pageNum,pageSize,orderBy);
+    }
 
 }
